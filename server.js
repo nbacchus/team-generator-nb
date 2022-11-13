9f1c2d4ec87d9c202a39adc8ea3f6cfdfe8330e6
Nadia Bacchus <nadiabacchus@Nadias-MacBook-Pro.local>
2022-11-13 17:04:10 -0500

    added user prompts

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
 const inquirer = require("inquirer");
 const cTable = require("console.table");
+const db = require("./db/connection");
+
+//user prompt questions
+const selectOption = [
+  {
+    type: "list",
+    name: "userSelected",
+    message: "Welcome to the Employee Manager! What would you like to do?",
+    choices: [
+      "View all employees",
+      "Add employee",
+      "Update employee role",
+      "View all roles",
+      "Add role",
+      "View all departments",
+      "Add department",
+    ],
+    default: "View all departments",
+  },
+];
 
