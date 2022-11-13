e7aafbbd8d9dddacac2d930a189c672c9e6cc1c4
Nadia Bacchus <nadiabacchus@Nadias-MacBook-Pro.local>
2022-11-13 17:35:33 -0500

    user selection function

--- server.js
+++ server.js
@@ -21,3 +21,53 @@ const selectOption = [
   },
 ];
 
+// Function to view all departments
+function viewAllDepartments() {
+    const sql = `
+    SELECT
+    id,
+    name AS 'Department'
+    FROM department
+    `;
+
+    db.query(sql, (err, department) => {
+        if (err) {
+            console.log(`There has been an error: ${err.sqlMessage}`)
+            return;
+        }
+
+        console.table(department);
+
+        // start inquirer over
+        init(selectOption)
+            .then(userSelectedObject => userSelected
+                (userSelectedObject.userSelected))
+            .catch((error) => {
+                console.log('Error', error);
+            });
+    });
+};
+
+// Switch statement to handle user selection
+function userSelected(input) {
+    switch (input) {
+        case 'View all departments':
+            return viewAllDepartments();
+        case 'View all roles':
+            return viewAllRoles();
+        case 'View all employees':
+            return viewAllEmployees();
+        case 'Add department':
+            return addNewDepartment();
+        case 'Add role':
+            return addNewRole();
+        case 'Add employee':
+            return addNewEmployee();
+        case 'Update employee role':
+            return updateEmployeeRole();
+        case 'Exit Employee Manager':
+            return exit();
+        default:
+            break;
+    }
+};
