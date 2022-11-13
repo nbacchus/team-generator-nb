856eb575606528ba94107fb0914423e445fb1c0b
Nadia Bacchus <nadiabacchus@Nadias-MacBook-Pro.local>
2022-11-13 17:43:39 -0500

    function to view, add and update employees

--- server.js
+++ server.js
@@ -21,22 +21,29 @@ const selectOption = [
   },
 ];
 
-// Function to view all departments
-function viewAllDepartments() {
+// Function to view all employees
+function viewAllEmployees() {
     const sql = `
     SELECT
-    id,
-    name AS 'Department'
-    FROM department
+    employee.id, 
+    employee.first_name AS 'First Name',
+    employee.last_name AS 'Last Name',
+    role.title AS 'Job Title',
+    department.name AS 'Department',
+    role.salary AS 'Salary',
+    employee.manager_id AS 'Manager'
+    FROM employee
+    LEFT JOIN role ON employee.role_id = role.id
+    LEFT JOIN department ON role.department_id = department.id
     `;
 
-    db.query(sql, (err, department) => {
+    db.query(sql, (err, employee) => {
         if (err) {
             console.log(`There has been an error: ${err.sqlMessage}`)
             return;
         }
 
-        console.table(department);
+        console.table(employee);
 
         // start inquirer over
         init(selectOption)
@@ -46,6 +53,130 @@ function viewAllDepartments() {
                 console.log('Error', error);
             });
     });
+};
+
+// Function to add new employee
+function addNewEmployee() {
+    getAllEmployees()
+        .then(([employeeRows]) => {
+
+            getAllRoles()
+                .then(([roleRows]) => {
+                    inquirer.prompt([
+                        {
+                            type: 'input',
+                            name: 'firstName',
+                            message: "What is the employee's first name?"
+                        },
+                        {
+                            type: 'input',
+                            name: 'lastName',
+                            message: "What is the employee's last name?"
+                        },
+                        {
+                            type: 'list',
+                            name: 'role',
+                            message: "What is the employee's role?",
+                            choices: roleRows.map(row => {
+                                return { name: row.title, value: row.id };
+                            })
+                        },
+                        {
+                            type: 'confirm',
+                            name: 'confirmManager',
+                            message: 'Does the employee have a manager?',
+                            default: false
+                        },
+                        {
+                            type: 'list',
+                            name: 'addManager',
+                            message: "Select the employee's manager from the list below.",
+                            choices: employeeRows.map(employee => {
+                                let name = employee.first_name.concat(' ', employee.last_name);
+                                return { name: name, value: employee.id };
+                            }),
+                            when: ({ confirmManager }) => {
+                                if (confirmManager) {
+                                    return true;
+                                } else {
+                                    return false;
+                                }
+                            }
+                        }
+                    ])
+                        .then(input => {
+                            const sql = `
+                INSERT INTO 
+                employee (first_name, last_name, role_id, manager_id) 
+                VALUES (?,?,?,?)`;
+                            db.query(sql, [input.firstName, input.lastName, input.role, input.addManager], (err) => {
+                                if (err) {
+                                    console.log(`There has been an error: ${err.sqlMessage}`)
+                                    return;
+                                }
+                                console.log(`Added ${input.firstName} ${input.lastName} to the database.`);
+                                // start inquirer over
+                                init(selectOption)
+                                    .then(userSelectedObject => userSelected
+                                        (userSelectedObject.userSelected))
+                                    .catch((error) => {
+                                        console.log('Error', error);
+                                    });
+                            });
+                        });
+                })
+        });
+};
+
+function updateEmployeeRole() {
+    getAllEmployees()
+        .then(([employeeRows]) => {
+            getAllRoles()
+                .then(([roleRows]) => {
+                    inquirer.prompt([
+                        {
+                            type: 'list',
+                            name: 'employee',
+                            message: "Which employee's role would you like to update?",
+                            choices: employeeRows.map(employee => {
+                                let name = employee.first_name.concat(' ', employee.last_name);
+                                return { name: name, value: employee.id };
+                            })
+                        },
+                        {
+                            type: 'list',
+                            name: 'updatedRole',
+                            message: 'Which role do you want to assign to the selected employee?',
+                            choices: roleRows.map(row => {
+                                return { name: row.title, value: row.id };
+                            })
+                        }
+                    ])
+                        .then(input => {
+                            const sql = `
+                        UPDATE employee 
+                        SET role_id = ? 
+                        WHERE id = ?`;
+
+                            db.query(sql, [input.updatedRole, input.employee], (err) => {
+                                if (err) {
+                                    console.log(`There has been an error: ${err.sqlMessage}`)
+                                    return;
+                                }
+
+                                console.log(`The employee's role has been updated.`);
+
+                                // start inquirer over
+                                init(selectOption)
+                                    .then(userSelectedObject => userSelected
+                                        (userSelectedObject.userSelected))
+                                    .catch((error) => {
+                                        console.log('Error', error);
+                                    });
+                            });
+                        });
+                });
+        });
 };
 
 // Switch statement to handle user selection
